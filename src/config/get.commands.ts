a2cc956fbda949e52ab8e3888f834650e61a6dfb
jpb06 <jpb.06@outlook.com>
2020-06-12 17:09:01 +0200

    Fixing typo

--- src/config/get.commands.ts
+++ src/config/get.commands.ts
@@ -18,28 +18,28 @@ export const getCommandsFor = (
         commands: config.appPreStartCommands,
         startText: "Executing pre start commands",
         errorText: "Pre start commands execution failure",
-        successTest: "Pre start commands execution failure",
+        successTest: "Pre start commands execution success",
       };
     case DeployStep.PostStart:
       return {
         commands: config.appPostStartCommands,
         startText: "Executing post start commands",
         errorText: "Post start commands execution failure",
-        successTest: "Post start commands execution failure",
+        successTest: "Post start commands execution success",
       };
     case DeployStep.PreStop:
       return {
         commands: config.appPreStopCommands,
         startText: "Executing pre stop commands",
         errorText: "Pre stop commands execution failure",
-        successTest: "Pre stop commands execution failure",
+        successTest: "Pre stop commands execution success",
       };
     case DeployStep.PostStop:
       return {
         commands: config.appPostStopCommands,
         startText: "Executing post stop commands",
         errorText: "Post stop commands execution failure",
-        successTest: "Post stop commands execution failure",
+        successTest: "Post stop commands execution success",
       };
   }
 };
