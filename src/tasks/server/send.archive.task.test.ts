3c3af4a1b1702ab1ba13d6243068d194b6f0833c
jpb06 <jpb.06@outlook.com>
2020-06-12 17:09:56 +0200

    Test coverage to one hundred

---
+++ src/tasks/server/send.archive.task.test.ts
@@ -0,0 +1,115 @@
+jest.mock("../../util/ssh.util");
+jest.mock("./../../util/console.util");
+jest.mock("../../util/logging.util");
+
+import { Console } from "./../../util/console.util";
+import { connect } from "../../util/ssh.util";
+import { mocked } from "ts-jest/utils";
+import { logError } from "../../util/logging.util";
+import {
+  dispose,
+  mkdir,
+  putFile,
+  mockSSHConnectForArchingSending,
+} from "./../../tests/ssh.connect.mock";
+import { config } from "../../tests/test.config";
+import { assignConsoleMocks } from "../../tests/console.mock";
+import { sendFileToDeployServer } from "./send.archive.task";
+
+assignConsoleMocks();
+
+describe("Send archive task", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should throw if connection failed", async () => {
+    mockSSHConnectForArchingSending(true, false, true);
+
+    try {
+      await sendFileToDeployServer(config, "source.zip", "/var/dest");
+    } catch (err) {
+      expect(err).toBe("Unable to send the archive");
+    }
+
+    expect(mocked(Console.StartTask).mock.calls).toHaveLength(1);
+    expect(mocked(Console.StartTask).mock.calls[0][0]).toEqual(
+      "Sending the archive to deploy server"
+    );
+
+    expect(mocked(Console.Success).mock.calls).toHaveLength(0);
+    expect(mocked(logError)).toHaveBeenCalled();
+
+    expect(mocked(mkdir)).toBeCalledTimes(0);
+    expect(mocked(putFile)).toBeCalledTimes(0);
+    expect(mocked(dispose)).toBeCalledTimes(0);
+  });
+
+  it("should throw if mkdir failed", async () => {
+    mockSSHConnectForArchingSending(true, false);
+
+    try {
+      await sendFileToDeployServer(config, "source.zip", "/var/dest");
+    } catch (err) {
+      expect(err).toBe("Unable to send the archive");
+    }
+
+    expect(mocked(Console.StartTask).mock.calls).toHaveLength(1);
+    expect(mocked(Console.StartTask).mock.calls[0][0]).toEqual(
+      "Sending the archive to deploy server"
+    );
+
+    expect(mocked(Console.Success).mock.calls).toHaveLength(0);
+    expect(mocked(logError)).toHaveBeenCalled();
+
+    expect(mocked(mkdir)).toHaveBeenCalled();
+    expect(mocked(putFile)).toBeCalledTimes(0);
+    expect(mocked(dispose)).toBeCalledTimes(1);
+  });
+
+  it("should throw if putFile failed", async () => {
+    mockSSHConnectForArchingSending(false, true);
+
+    try {
+      await sendFileToDeployServer(config, "source.zip", "/var/dest");
+    } catch (err) {
+      expect(err).toBe("Unable to send the archive");
+    }
+
+    expect(mocked(Console.StartTask).mock.calls).toHaveLength(1);
+    expect(mocked(Console.StartTask).mock.calls[0][0]).toEqual(
+      "Sending the archive to deploy server"
+    );
+
+    expect(mocked(Console.Success).mock.calls).toHaveLength(0);
+    expect(mocked(logError)).toHaveBeenCalled();
+
+    expect(mocked(mkdir)).toBeCalledTimes(1);
+    expect(mocked(putFile)).toBeCalledTimes(1);
+    expect(mocked(dispose)).toBeCalledTimes(1);
+  });
+
+  it("should complete gracefully if task succeeds", async () => {
+    mockSSHConnectForArchingSending(false, false);
+
+    expect(await sendFileToDeployServer(config, "source.zip", "/var/dest"))
+      .resolves;
+
+    expect(mocked(connect).mock.calls).toHaveLength(1);
+    expect(mocked(Console.StartTask).mock.calls).toHaveLength(1);
+    expect(mocked(Console.StartTask).mock.calls[0][0]).toEqual(
+      "Sending the archive to deploy server"
+    );
+
+    expect(mocked(Console.Success).mock.calls).toHaveLength(1);
+    expect(mocked(Console.Success).mock.calls[0][0]).toEqual(
+      "Archive uploaded to deploy server"
+    );
+
+    expect(mocked(logError)).toBeCalledTimes(0);
+
+    expect(mocked(mkdir)).toBeCalledTimes(1);
+    expect(mocked(putFile)).toBeCalledTimes(1);
+    expect(mocked(dispose)).toBeCalledTimes(1);
+  });
+});
