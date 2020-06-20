540c44277bd2524a506373e723f8c751403169e3
jpb06 <jpb.06@outlook.com>
2020-06-20 18:43:33 +0200

    Main prop in package.json shouldn't be validated for SPAs

--- src/tasks/config/load.package.file.task.ts
+++ src/tasks/config/load.package.file.task.ts
@@ -3,7 +3,7 @@ import { logError } from "../../util/logging.util";
 import { Console } from "../../util/console.util";
 import { pathExists } from "fs-extra";
 
-export const loadPackageFile = async (): Promise<any> => {
+export const loadPackageFile = async (isSpa: boolean): Promise<any> => {
   try {
     Console.StartTask("Checking package.json");
     const packageFile = "./package.json";
@@ -13,8 +13,12 @@ export const loadPackageFile = async (): Promise<any> => {
 
     const json = await readJSON(packageFile);
 
-    if (!json.name || !json.version || !json.main) {
-      throw "the package.json file has missing properties: name, version and main must be defined.";
+    if (!json.name || !json.version || (!isSpa && !json.main)) {
+      const message = "the package.json file has missing properties:";
+      const requiredProps = isSpa
+        ? "name and version"
+        : "name, version and main";
+      throw `${message} ${requiredProps} must be defined`;
     }
 
     Console.Success("package.json content extracted");
