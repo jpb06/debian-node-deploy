b98bf27ff5452391fa59672da2fd13e7ec2172de
jpb06 <jpb.06@outlook.com>
2021-01-10 16:16:48 +0100

    Getting rid of the copy package function

--- project-utils/scripts/copy.package.json.file.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs-extra');
-
-fs.copy('./package.json', './dist-deploy/package.json', err => {
-   if (err) return console.log(err);
-
-   console.log("package.json file copied to dist folder");
-});
